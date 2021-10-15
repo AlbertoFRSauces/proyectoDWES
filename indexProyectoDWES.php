c442a507f517e60d2f629597001de9fb55f50d60
Alberto <alberto.ferram@educa.jcyl.es>
2021-10-15 15:48:38 +0200

    15/10/2021 v1.Commit proyectoDWES.

---
+++ indexProyectoDWES.php
@@ -0,0 +1,130 @@
+<!DOCTYPE html>
+<html lang="es">
+    <head>
+        <title>Web Alberto Fernandez Ramirez</title>
+        <meta charset ="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">       
+        <meta name="author" content="Alberto Fernandez Ramirez">
+        <meta name="application-name" content="Proyecto 2021 DAW">
+        <meta name="description" content="Mi pagina web DAW">
+        <meta name="keywords" content="analítica web, seo, web semántica, seo semántico, analytics" >
+        <meta name="robots" content="index, follow">
+        <link href="webroot/css/estilo.css" rel="stylesheet" type="text/css">
+        <link rel="icon" href="../img/home.png" type="image/x-icon">
+    </head>
+    <body>
+        <header>
+            <h1>Desarrollo de Aplicaciones Web</h1>
+        </header>
+        <article class="segundot">
+            <h2>Desarrollo Web Entorno Servidor</h2>
+        </article>
+        <main class="container">
+            <section class="primerasfilas">
+                <table class="tabla">
+                    <tr class="bordetitulo">
+                        <td>
+                            <figure>
+                                <a href="doc/EstudioTema1.pdf" target="_blank"><figcaption class="titulofoto">TEMA 1: DESARROLLO WEB EN ENTORNO SERVIDOR</figcaption></a>
+                            </figure>
+                        </td>
+                    </tr>
+                </table>
+            </section>
+            <section class="primerasfilas">
+                <table class="tabla">
+                    <tr class="bordetitulo">
+                        <td>
+                            <figure>
+                                <a href="doc/EstudioTema2.pdf" target="_blank"><figcaption class="titulofoto">TEMA 2: INSTALACIÓN, CONFIGURACIÓN Y DOCUMENTACIÓN DEL ENTORNO DE DESARROLLO Y DEL ENTORNO DE EXPLOTACIÓN</figcaption></a>
+                            </figure>
+                        </td>
+                    </tr>
+                </table>
+            </section>
+            <section class="primerasfilas">
+                <table class="tabla">
+                    <tr class="bordetitulo">
+                        <td>
+                            <figure>
+                                <a href="../proyectoTema3/indexProyectoTema3.php"><figcaption class="titulofoto">TEMA 3: CARACTERÍSTICAS DEL LENGUAJE PHP</figcaption></a>
+                            </figure>
+                        </td>
+                    </tr>
+                </table>
+            </section>
+            <section class="primerasfilas">
+                <table class="tabla">
+                    <tr class="bordetitulo">
+                        <td>
+                            <figure>
+                                <a href="../webroot/css/img/imgconstruction.jpg"><figcaption class="titulofoto">TEMA 4: TÉCNICAS DE ACCESO A DATOS EN PHP</figcaption></a>
+                            </figure>
+                        </td>
+                    </tr>
+                </table>
+            </section>
+            <section class="primerasfilas">
+                <table class="tabla">
+                    <tr class="bordetitulo">
+                        <td>
+                            <figure>
+                                <a href="../webroot/css/img/imgconstruction.jpg"><figcaption class="titulofoto">TEMA 5: DESARROLLO DE APLICACIONES WEB UTILIZANDO CÓDIGO EMBEBIDO</figcaption></a>
+                            </figure>
+                        </td>
+                    </tr>
+                </table>
+            </section>
+            <section class="primerasfilas">
+                <table class="tabla">
+                    <tr class="bordetitulo">
+                        <td>
+                            <figure>
+                                <a href="../webroot/css/img/imgconstruction.jpg"><figcaption class="titulofoto">TEMA 6: PROGRAMACIÓN ORIENTADA A OBJETOS EN PHP</figcaption></a>
+                            </figure>
+                        </td>
+                    </tr>
+                </table>
+            </section>
+            <section class="primerasfilas">
+                <table class="tabla">
+                    <tr class="bordetitulo">
+                        <td>
+                            <figure>
+                                <a href="../webroot/css/img/imgconstruction.jpg"><figcaption class="titulofoto">TEMA 7: PROGRAMACIÓN DE SERVICIOS WEB</figcaption></a>
+                            </figure>
+                        </td>
+                    </tr>
+                </table>
+            </section>
+            <section class="primerasfilas">
+                <table class="tabla">
+                    <tr class="bordetitulo">
+                        <td>
+                            <figure>
+                                <a href="../webroot/css/img/imgconstruction.jpg"><figcaption class="titulofoto">TEMA 8: DESARROLLO DE APLICACIONES WEB HÍBRIDAS</figcaption></a>
+                            </figure>
+                        </td>
+                    </tr>
+                </table>
+            </section>
+            <section class="primerasfilas">
+                <table class="tabla">
+                    <tr class="bordetitulo">
+                        <td>
+                            <figure>
+                                <a href="../webroot/css/img/imgconstruction.jpg"><figcaption class="titulofoto">TEMA 9: DESARROLLO DE APLICACIONES WEB HÍBRIDAS</figcaption></a>
+                            </figure>
+                        </td>
+                    </tr>
+                </table>
+            </section>
+        </main>
+        <footer class="piepagina">
+            <a href="../index.php"><img src="../webroot/css/img/atras.png" class="imageatras" alt="IconoAtras" /></a>
+            <a href="https://github.com/AlbertoFRSauces/proyectoDAW/tree/master/proyectoDWES" target="_blank"><img href="https://github.com/AlbertoFRSauces/proyectoDAW/tree/master/proyectoDWES" target="_blank" src="../webroot/css/img/github.png" class="imagegithub" alt="IconoGitHub" /></a>
+            <p><a>&copy;</a>Alberto Fernández Ramírez 29/09/2021 Todos los derechos reservados.</p>
+            <p>Ultima actualización: 13/10/2021 18:59</p>
+        </footer>
+    </body>
+</html>
